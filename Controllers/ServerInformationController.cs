f3cf74130b14c1e829da4a63314f56dfab66fd84
Mathis Burger <handymathis@gmail.com>
2022-01-27 08:18:16 +0100

    [TASK] added server information controller

---
+++ Controllers/ServerInformationController.cs
@@ -0,0 +1,14 @@
+﻿using digify.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace digify.Controllers;
+
+[ApiController]
+public class ServerInformationController : ControllerBase
+{
+    [HttpGet("/api/server-information")]
+    public ActionResult<ServerInformation> Index()
+    {
+        return new ServerInformation("v1");
+    }
+}
---
+++ Models/ServerInformation.cs
@@ -0,0 +1,13 @@
+﻿namespace digify.Models;
+
+public class ServerInformation
+{
+    public string ApiVersion { get; }
+    public DateTime CurrentDate { get; }
+
+    public ServerInformation(string apiVersion)
+    {
+        this.ApiVersion = apiVersion;
+        this.CurrentDate = new DateTime();
+    }
+}
