e1338ec9d74236391786148d353e84d56963be4c
Mathis Burger <handymathis@gmail.com>
2022-01-28 08:56:51 +0100

    [TASK] added me endpoint

---
+++ Controllers/UserController.cs
@@ -0,0 +1,12 @@
+﻿using digify.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace digify.Controllers;
+
+[ApiController]
+public class UserController : AuthorizedControllerBase
+{
+
+    [HttpGet("/api/user/me")]
+    public ActionResult<User> Me() => Ok(AuthorizedUser);
+}
