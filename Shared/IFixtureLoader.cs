549752217cb86cb42bdb79e8cb50f319e164b0dd
Mathis <handymathis@gmail.com>
2022-02-06 00:30:42 +0100

    [DOCS] documented files

--- Shared/IFixtureLoader.cs
+++ Shared/IFixtureLoader.cs
@@ -1,6 +1,13 @@
 ﻿namespace digify.Shared;
 
+/// <summary>
+/// Scaffold for fixture loader
+/// </summary>
 public interface IFixtureLoader
 {
+    /// <summary>
+    /// Loads all fixtures
+    /// </summary>
+    /// <param name="context">The database connection</param>
     Task Load(IContext context);
 }
--- Util/CryptoRandom.cs
+++ Util/CryptoRandom.cs
@@ -2,8 +2,16 @@
 
 namespace digify.Util;
 
+/// <summary>
+/// A collections of utils creating random values
+/// </summary>
 public class CryptoRandom
 {
+    /// <summary>
+    /// Creates an array of random bytes
+    /// </summary>
+    /// <param name="size">The number of random bytes</param>
+    /// <returns>The array of random bytes</returns>
     public static byte[] GetBytes(uint size)
     {
         var bytes = new byte[size];
