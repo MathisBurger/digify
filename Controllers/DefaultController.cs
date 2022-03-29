ff2b18aa33a26f90d0fc9251ccc4b69fb55bec3d
Mathis Burger <handymathis@gmail.com>
2022-03-29 10:02:56 +0200

    [TASK] prepared application for production

---
+++ Controllers/DefaultController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace digify.Controllers;
+[ApiController]
+public class DefaultController : ControllerBase
+{
+    [HttpGet("/")]
+    public IActionResult EmptyRoute() => new RedirectResult("/dashboard");
+
+    [HttpGet("{*url}")]
+    public IActionResult DefaultRedirect([FromRoute] string url)
+    {
+        if (System.IO.File.Exists("./wwwroot/" + url))
+        {
+            return File("~/" + url, "text/html");
+        }
+
+        return File("~/index.html", "text/html");
+    }
+    
+}
---
+++ Dockerfile
@@ -0,0 +1,19 @@
+﻿FROM mcr.microsoft.com/dotnet/core/aspnet:6.0-buster-slim AS base
+WORKDIR /app
+EXPOSE 80
+EXPOSE 443
+
+FROM mcr.microsoft.com/dotnet/core/sdk:6.0-buster AS build
+WORKDIR /src
+COPY "digify.csproj" . 
+RUN dotnet restore "digify.csproj"
+COPY . .
+RUN dotnet build "digify.csproj" -c Release /app/build
+
+FROM build as publish
+RUN dotnet publish "digify.csproj" -c Release /app/publish
+
+FROM base as final
+WORKDIR /app
+COPY --from=publish /app/publish .
+ENTRYPOINT ["dotnet", "digify.dll"]
